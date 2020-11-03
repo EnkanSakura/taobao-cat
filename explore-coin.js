08cb4abee4060d131593ae1de2194c2c4920e683
EnkanSakura <EnkanSakura@users.noreply.github.com>
2020-11-03 16:08:34 +0800

    enhance

--- explore-coin.js
+++ explore-coin.js
@@ -1,22 +1,44 @@
+var 去浏览;
+var 喵币;
+var 全部完成 = false;
+
 sleep(1000);
-var target_text = "去浏览";
-var progress_text = "/20)";
-var go_explore;
-while (go_explore = textContains(target_text).findOne(100)) {
-    var waitTimes = 0;
-    go_explore.click();
+toastLog("开始");
+while (true) {
+    去浏览 = textContains("去浏览").findOne(100);
+    if (!去浏览) {
+        全部完成 = true;
+        var 可浏览 = textContains("秒").find();
+        for (var i = 0; i < 可浏览.length; i++) {
+            去浏览 = 可浏览[i].parent().parent().child(1);
+            if (去浏览.text() != "已完成") {
+                全部完成 = false;
+                break;
+            }
+        }
+    }
+    if (全部完成) {
+        break;
+    }
+    var 重试次数 = 0;
+    去浏览.click();
     sleep(20000);
     while (!descContains("完成").findOne(100) &&
         !textContains("完成").findOne(100) &&
-        !textContains(/[0-9]0000/).findOne(100)) {
+        !textContains(/[0-9][0-9]000/).findOne(100)) {
         sleep(1000);
-        waitTimes++;
-        if (waitTimes > 5) {
+        重试次数++;
+        if (重试次数 > 5) {
             break;
         }
     }
-    waitTimes = 0;
+    重试次数 = 0;
     back();
     sleep(2000);
 }
 toast("完成");
+var 领取奖励 = textContains("领取奖励").find()
+for (var i = 0; i < 领取奖励.length; i++) {
+    领取奖励[i].click();
+    sleep(200);
+}
